585f5485f90cf041472db8cc8055bba4b3530628
pooja115 <57281040+pooja115@users.noreply.github.com>
2019-11-02 14:39:40 +0530

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -3,10 +3,13 @@ class Customer
   int custId;
   String custName;
   
-  public int get custIden()
+  public int getcustId()
   {
     int a=1;
     return a;
+  }
+  public setcustId()
+  {
   }
 }
 //just for sample purpose 
